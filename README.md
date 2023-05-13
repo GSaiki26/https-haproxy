6e351071db86d07d0cc273ddcdd28e6913c6eb2d
Saiki <GSaiki26@gmail.com>
2023-05-13 19:42:32 -0300

    Added: GRPC with TLS (not working yet)

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ This repository has the intent to provide a simples example of a HTTPS server us
 The server was made in NodeJS.
 
 Just select an example and checkout the README.md on each one of then.
+* [GRPC NOAUTH NOTLS](https://github.com/GSaiki26/https-haproxy/tree/master/examples/grpc-no-tls)
+* [HTTPS AUTH](https://github.com/GSaiki26/https-haproxy/tree/master/examples/grpc-no-tls)
+* [HTTPS NOAUTH](https://github.com/GSaiki26/https-haproxy/tree/master/examples/grpc-no-tls)
---
+++ examples/grpc-tls/server/src/index.ts
@@ -0,0 +1,40 @@
+// Libs
+import { readFileSync } from "fs";
+
+import * as grpc from "@grpc/grpc-js";
+import { loadSync } from "@grpc/proto-loader";
+
+import ToolsService from "./services/toolsService";
+
+// Data
+const HOST = "0.0.0.0:8000";
+const server = new grpc.Server();
+const toolsProto = loadSync("./src/proto/tools.proto");
+const toolsPackage: any = grpc.loadPackageDefinition(toolsProto).tools;
+
+// Routes
+server.addService(toolsPackage.ToolsService.service, {
+  Ping: ToolsService.ping.bind(ToolsService)
+});
+
+// Define the credentials.
+const creds = grpc.ServerCredentials.createSsl(
+    readFileSync("./certs/ca.pem"),
+    [
+      {
+        private_key: readFileSync("./certs/server.pem.key"),
+        cert_chain: readFileSync("./certs/server.pem"),
+      }
+    ], true
+);
+
+// Start the server.
+server.bindAsync(HOST, creds, (err, port) => {
+  if (err) {
+    console.error(`Error while trying to start the server. Error: ${err}`);
+    return;
+  }
+
+  server.start();
+  console.info(`The server is online on port: ${port}`);
+});
